ffb0e239c7b45bae45b9ed0defd1b79d94c47817
dweebit <brakit@gmail.com>
2015-08-02 15:38:44 -0700

    Verify that a default nginx site is not being created

--- test/integration/default/serverspec/web_app_spec.rb
+++ test/integration/default/serverspec/web_app_spec.rb
@@ -8,3 +8,13 @@ end
 describe port(80) do
   it { should be_listening }
 end
+
+nginx_sites_enabled_path = "/etc/nginx/sites-enabled/"
+
+describe file(nginx_sites_enabled_path) do
+  it { should be_directory }
+end
+
+describe command("ls #{nginx_sites_enabled_path}") do
+  its(:stdout) { should eq "" }
+end
