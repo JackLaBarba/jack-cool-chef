17eb78ec9101aa8c28a567e4dc12486fa1637169
dweebit <brakit@gmail.com>
2015-08-02 10:47:59 -0700

    Run default recipes for apt and nginx.
    Needed apt to get up-to-date packages

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,6 @@ license          'All rights reserved'
 description      'Installs/Configures jack_cool'
 long_description 'Installs/Configures jack_cool'
 version          '0.1.0'
+
+depends 'apt'
+depends 'nginx'
